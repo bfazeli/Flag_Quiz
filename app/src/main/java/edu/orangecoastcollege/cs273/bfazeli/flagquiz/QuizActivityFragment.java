886abac3773b0f7cd7632e6bef72292552fe4497
bijan102 <bfazeli1@gmail.com>
2016-09-27 12:34:12 -0700

    Declared private fields for QuizActivityFragment

--- app/src/main/java/edu/orangecoastcollege/cs273/bfazeli/flagquiz/QuizActivityFragment.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/bfazeli/flagquiz/QuizActivityFragment.java
@@ -5,14 +5,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Handler;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class QuizActivityFragment extends Fragment {
 
-    public QuizActivityFragment() {
-    }Q
+    // String used when logging error messages
+    private static final String TAG = "FlagQuiz Activity";
+
+    private static final int FLAGS_IN_QUIZ = 10;
+
+    private List<String> fileNameList; // flag file names
+    private List<String> quizCountriesList; // countries in current quiz
+    private Set<String> regionsSet; // world regions in current quiz
+    private String correctAnswer; // correct country for the current flag
+    private int totalGuesses; // number of quesses made
+    private int correctAnswers; // number of correct guesses
+    private int quessRows; // number of rows displaying guess Buttons
+    private SecureRandom random; // used to randomize the quiz
+    private Handler handler; // used to delay loading next flag
+
+
+    private TextView questionNumberTextView; // shows current question #
+    private ImageView flagImageView; // displays a flag
+    private LinearLayout[] guessLinearLayouts; // rows of answer Buttons
+    private TextView answerTextView; // displays correct answer
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
